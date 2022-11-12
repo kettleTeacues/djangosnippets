9a1ff57f98c222a74a49bf7cd1131b2a7a55f3fd
kettle <teacues@gmail.com>
2022-11-12 23:18:50 +0900

    詳細画面を追加

--- snippets/tests.py
+++ snippets/tests.py
@@ -58,3 +58,26 @@ class EditSnippetTest(TestCase):
     def test_should_resolve_snippet_edit(self):
         found = resolve('/snippets/1/edit/')
         self.assertEqual(snippet_edit, found.func)
+
+class SnippetDetailTest(TestCase):
+    def setUp(self):
+        self.user = UserModel.objects.create(
+            username = 'test_user',
+            email = 'test@example.com',
+            password = 'secret',
+        )
+        self.snippet = Snippet.objects.create(
+            title = 'タイトル',
+            code = 'コード',
+            description = '解説',
+            created_by = self.user,
+        )
+
+    def test_should_use_expected_template(self):
+        response = self.client.get('/snippets/%s/' % self.snippet.id)
+        self.assertTemplateUsed(response, 'snippets/snippet_detail.html')
+    
+    def test_top_page_returns_200_and_expected_heading(self):
+        response = self.client.get('/snippets/%s/' % self.snippet.id)
+        self.assertContains(response, self.snippet.title, status_code=200)
+
