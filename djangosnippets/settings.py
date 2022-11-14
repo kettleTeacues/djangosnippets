9b486045deda9e083a139c9946779eb687012fa9
kettle <teacues@gmail.com>
2022-11-14 11:21:28 +0900

    ログイン、ログアウトの実装

--- djangosnippets/settings.py
+++ djangosnippets/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'snippets',
+    'accounts',
     'django_bootstrap5',
     'pygments_renderer',
 ]
@@ -126,3 +127,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_URL = '/accounts/login'
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
